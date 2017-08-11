99d80ce8e325bbb5b100fafdfa56848611efe1be
ZennoPy <zennopy@yandex.ru>
2017-08-11 10:51:46 +0600

    Исправлена ошибка: если не указать путь до файла, скрипт сломается

--- pprint_json.py
+++ pprint_json.py
@@ -3,25 +3,21 @@ import sys
 import os.path
 
 
-# Функция загрузки Json из файла
 def load_data(filepath):
-
-    # Если указанный файл существует, то получаем данные из файла
-    # Иначе прерываем работу скрипта
     if os.path.isfile(filepath):
         with open(filepath) as json_file:
             return json.load(json_file)
     else:
-        return "Указанного файла  не существует"
+        return "The specified file does not exist"
 
 
-# Функция для приведения Json в читаемый вид
 def pretty_print_json(json_data):
     return json.dumps(json_data, indent=4, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    # Получаем имя json файла из командной строки
-    location_file = sys.argv[1]
-    # Приводим json в читаемый вид
-    print(pretty_print_json(load_data(location_file)))
+    try:
+        location_file = sys.argv[1]
+        print(pretty_print_json(load_data(location_file)))
+    except IndexError:
+        print("Not specified json file")
